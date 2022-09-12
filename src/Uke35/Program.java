2d47f48587b86492c2e14901f7e60e6580db7004
hamidhamrah <s362052@oslomet.no>
2022-09-12 12:05:46 +0200

    Er ferdig med UKe 35 og har begynt på UkesOppgaver 36

--- src/Uke35/Program.java
+++ src/Uke35/Program.java
@@ -1,5 +1,6 @@
 package Uke35;
 
+import java.util.Arrays;
 import java.util.Random;
 
 
@@ -14,26 +15,49 @@ public class Program {
 
       //  System.out.println("\nStørste verdi ligger på plass " + m);
        // int[] a = Tabell.randPerm(10);
-        int[] c = null;
+        //int[] c = null;
 
       //  Tabell.maks2(a,10,10);
 
-        int[] a = Tabell.randPerm(10); // tilfeldig permutasjon av 1 . . 20
-        int[] b = Tabell.nestMaks3(a);  // metoden returnerer en tabell
+     //   int[] a = Tabell.randPerm(10); // tilfeldig permutasjon av 1 . . 20
+     //   int[] b = Tabell.nestMaks3(a);  // metoden returnerer en tabell
 
-        int m = b[0], nm = b[1];       // m for maks, nm for nestmaks
+       // int m = b[0], nm = b[1];       // m for maks, nm for nestmaks
 
       //  Tabell.skriv2(a);        // se Oppgave 5 i Avsnitt 1.2.2
        // System.out.print("Størst(" + a[m] + ") har posisjon " + m);
       //  System.out.print(", nest størst(" + a[nm] + ") har posisjon " + nm);
-        for (int i:a){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        Tabell.sortering(a);
-        for (int i:a){
-            System.out.print(i+" ");
-        }
+        //for (int i:a){
+       //     System.out.print(i+" ");
+     //   }
+      //  System.out.println();
+      //  Tabell.sortering(a);
+     //   for (int i:a){
+     //       System.out.print(i+" ");
+     //   }
+        int[] d = {2, 3, 6, 1 ,4 ,5};
+        System.out.println(Arrays.toString(d));// permutasjon av tallene fra 1 til 10
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+        Tabell.nestePermutasjon(d);              // lager neste permutasjon
+        System.out.println(Arrays.toString(d));
+
     }
 
 }
