714b16af4a54bad8a9d1937c725c76222bbf9c4f
hamidhamrah <s362052@oslomet.no>
2022-09-27 15:34:08 +0200

    Har satrtet Ukes Oppgave 38. Holder på med Oppgve 1.5.1 9

--- src/Uke35/Program.java
+++ src/Uke35/Program.java
@@ -2,6 +2,7 @@ package Uke35;
 
 import java.awt.*;
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 public class Program {
 
@@ -176,9 +177,69 @@ public class Program {
         System.out.println(Arrays.toString(s));
     */
 
+      int [] a={1,2,3,4,5,6,7,8};
+        System.out.println(maks(a));
 
 
 
+    }
+    public static int a(int n)           // n må være et ikke-negativt tall
+    {
+     if (n<0){
+        throw new NoSuchElementException("N er negative");
+     }
+     int x=0,y=1,z=1;
+        for (int i=0; i<n; i++){
+            z=2*y+3*x;
+            x=y;
+            y=z;
+        }
+     return z;
+    }
+    public static int tverrsumItrativt(int n)              // n må være >= 0
+    {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+    public static int tverrsum(int n)              // n må være >= 0
+    {
+        if (n < 10) return n;                        // kun ett siffer
+      while (n>10){
+        n=tverrsumItrativt(n);
+      }
+      return n;
+    }
+    public static int Kvadratt(int n){
+      if (n==1){
+          return 1;
+      }
+      else {
+          return Kvadratt(n-1)+n*n;
+      }
+    }
+    public static int sum(int fra, int til){
+        //0+1+2+3+4
+        if (fra==til) {
+            return fra;
+        }
+        int m=(fra+til)/2;
+        return sum(fra,m)+sum(fra+1,til);
+    }
+    public static int maks(int[] a, int fra, int til){
+        int m=(fra+til)/2;
+        if (fra==til)return fra;
+        int mFra=maks(a,m+1,til);
+        int mTil=maks(a,fra,m);
+        return a[mFra]>=a[mTil]? mFra:mTil;
+    }
+    public static int maks(int[] a){
+           return maks(a,0,a.length-1);
     }
 
 }
